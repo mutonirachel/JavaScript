d231cf3983a51d5fad321bf7c0be2ba33a03e1a3
mutonirachel <mutonirachel20@gmail.com>
2023-12-01 09:43:56 +0200

    datatypes

--- day-5/main.js
+++ day-5/main.js
@@ -41,7 +41,80 @@ console.log("height"); // height
 height = 180;
 console.log(height); // 180
 
+// Let's modify our example by using strict:
+// We use it when we want to force the interpreter to behave according to modern JavaScript standards.
+
+"use strict";
+
+height = 180;   // Uncaught ReferenceError: height is not defined
+console.log(height);
+
+// changing variable values 
+
+let steps = 100;
+console.log(steps); // 100
+steps = 120;  
+console.log(steps);    // 120
+steps = steps + 200;
+console.log(steps);  // 320
+
+// declare and initialize variables of different types (data types)
+
+let greeting = "Hello";  // the greeting variable is initiated with a value of the string type
+let counter = 100;  // the counter variable is initiated with a value of the number type
+
+console.log(greeting);  // Hello
+greeting = 1;
+console.log(greeting);  // 1
+
+// JavaScript allows us to easily replace the greeting variable with a value whose type is different from the one originally stored there.
+// allows us to change the types of values kept in a variable, but it also performs their implicit conversion if necessary 
 
 
+let greetings = "Hello!";  
+let counters = 100;
 
+greetings = greetings + counter;
+console.log(greetings);  // Hello!100
+
+
+// CONSTANTS
+
+const texts; // Uncaught SyntaxError: Missing initializer in const declaration
+texts = "Hello!";
+
+
+const text = "Hello";  // it's correct because it has declaration and initialiations
+
+// again this is wrong
+// a change in the constant is impossible.
+//  declaration is correct, but we try to modify the value stored in the constant.
+
+const devclass = "Front-End";
+devclass = "Back-End";  // Uncaught  TypeError:  Assignment  to  constant  variable.
+
+
+// SCOPE
+
+let cont;
+console.log(cont);  // undefined
+{
+    cont = 1;
+    console.log(cont);  // 1
+}
+cont = cont + 1;
+console.log(cont);  // 2
+
+// Program blocks can be nested, that is, we can create one block inside of another one.
+
+let contr;
+console.log(contr);  // undefined
+{
+    cont = 1;
+    {
+        console.log(contr);  // 1
+    }
+}
+cont = cont + 1;
+console.log(contr);  // 2
 
---
+++ day-6/main.js
@@ -0,0 +1,16 @@
+// DATATYPES
+
+// a = 5 + "9";
+// document.write(a);
+
+(function(){
+    setTimeout(()=> console.log(1),2000);
+    console.log(2);
+    setTimeout(()=> console.log(3),0);
+    console.log(4);
+   })();
+
+
+   let a = 2;
+   let a = "first";   // Error a is alreay declared
+   console.log(a);
